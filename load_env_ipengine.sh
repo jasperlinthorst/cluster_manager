0920498a87105256f981de43f2fec4b5b7cfbaaa
Alexey Gritsenko <grizenko.a@gmail.com>
2014-02-28 08:56:34 +0100

    Also added commands for csh

--- load_env_ipengine.sh
+++ load_env_ipengine.sh
@@ -17,7 +17,9 @@ sleep $WAIT
 echo "Waiting period over for $PARAMNR, starting bootstrap."
 
 echo "Running ulimit"
-ulimit
+ulimit;
+limit stacksize unlimited;
+limit memoryuse unlimited;
 
 #load environment
 chmod 744 ./gcp
